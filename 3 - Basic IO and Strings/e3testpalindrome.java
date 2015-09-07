5205d66fda2403f1caf15c8f279b2e00e72adac1
mdnu <mark@sdentertainment.ca>
2015-09-07 04:00:24 -0400

    Added e3testpalindrome.java

---
+++ 3 - Basic IO and Strings/e3testpalindrome.java
@@ -0,0 +1,35 @@
+// Checks if the input String is a palindrome.
+
+import java.util.Scanner;
+
+class e3testpalindrome {
+	
+	static String regularExpression = "[^A-z]";
+	
+	public String reverseStr(String exStr) {
+		int exStrLen = exStr.length();
+		String outStr = ""; // output String.
+		for (int i = exStrLen-1; i>=0; i--) {
+			outStr = outStr+exStr.charAt(i);
+		} return outStr;
+	}
+	
+	public static void main(String[] args) {
+		String inStr; // input String.
+		e3testpalindrome im = new e3testpalindrome();
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a phrase/word: ");
+		inStr = in.nextLine(); // Get unmodified String.
+		String exStr = inStr.toLowerCase().replaceAll(regularExpression, "");
+		// Create modified String.
+		String outStr = im.reverseStr(exStr);
+		if (exStr.equals(outStr)) {
+			System.out.println(inStr+" is a palindrome.");
+		} else {
+			System.out.println(inStr+" is not a palindrome.");
+		}
+		
+	}
+	
+}
