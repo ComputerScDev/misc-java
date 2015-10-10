8b8bff5786c3968abfa8d964762b27aa91c5d4fa
mdnu <mark@sdentertainment.ca>
2015-10-09 22:28:38 -0400

    Added CountNumber program.

---
+++ CountNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class CountNumber {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String line = in.nextLine();
+		
+		int spaceCount = 0;
+		for (char c : line.toCharArray()) {
+			if (c == ' ') {
+				spaceCount++;
+			}
+		}
+		
+		System.out.println(spaceCount+1);
+	}
+	
+}
