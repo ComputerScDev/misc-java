b9ae847eb5b48cb3b572b43378a7a1478d1e783a
mdnu <mdnu@users.noreply.github.com>
2015-11-15 22:13:21 -0500

    Update DistanceV5.java

--- DistanceCalculatorOOP/DistanceV5.java
+++ DistanceCalculatorOOP/DistanceV5.java
@@ -1,11 +1,9 @@
 public class DistanceV5 {
 	
 	public static void main(String[] args) {
-		
 		DistanceCalculator dc = new DistanceCalculator();
 		dc.getUserInput();
 		dc.printDistance();
-		
 	} // end of main function
 	
 } // end of class
