036cc8f1e7abd0848cc9b3b1a47083c449ff4eed
mdnu <mdnu@users.noreply.github.com>
2015-11-20 23:11:42 -0500

    Update fastExponentiation.java

--- 07 - Difficult Exercises/fastExponentiation.java
+++ 07 - Difficult Exercises/fastExponentiation.java
@@ -51,20 +51,3 @@ class fastExponentiation {
 
 /* Given rational variables x,z and natural variable, write a program 
 for z' = x^y that runs fast without using exponentiation. */
-
-
-/* (z'=x^y) <= z:=1 . P
-P <= if (even y) then (even y =>P) else (odd y => P) fi
-(even y => P) <= if (y=0) then ok else (even y && y>0 => P) fi
-(odd y => P) <= z:= z*x . y:= y-1 . (even y => P)
-(even y && y>0 => P) <= x:=x*x . y:=y/2 . (y>0 => P)
-(y>0 => P) <= if (even y) then (even y && y>0 => P) else (odd y => P) fi
-
-Compiler view:
-
-A	<= z:=1 . B
-B	<= if (even y) then C else D fi
-C	<= if (y=0) then ok else E fi
-D	<= z:= z*x . y:= y-1 . C
-E	<= x:=x*x . y:=y/2 . F
-F	<= if (even y) then E else D fi */
