a5ebd4410999095a8742972e6b672cfb358cc43f
mdnu <mark@sdentertainment.ca>
2016-04-04 23:43:20 -0400

    LineSub subclass of Point superclass (incomplete).

---
+++ Exercises/11 - Composition vs. Inheritance/Example - Points and Lines/LineSub.java
@@ -0,0 +1,21 @@
+//	Inheritance example
+//	"A line is a point extended by another point"
+//	LineSub line subclass of Point superclass
+//	-end:Point
+//	+LineSub(x1:int, y1:int, x2:int, y2:int)
+//	+getBegin():Point
+//	+setBegin(begin:Point):void
+//	+getBeginX():int
+//	+setBeginX(x:int):void
+//	+getBeginY():int
+//	+setBeginY(y:int):void
+//	+setBeginXY(x:int, y:int):void
+//	+getEnd():Point
+//	+setEnd(end:Point):void
+//	+getEndX():int
+//	+setEndX(x:int):void
+//	+getEndY():int
+//	+setEndY(y:int):void
+//	+setEndXY(x:int, y:int):void
+//	+toString():String
+//	+getLength():double
