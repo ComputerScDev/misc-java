0cd2ba81ddb4dccc0abd9d5892aa67afdfc092c9
mdnu <mark@sdentertainment.ca>
2016-04-22 14:42:40 -0400

    Code cleaning.

--- Exercises/01 - Flow Controls/1.2 - Loop/e5tfs.java
+++ Exercises/01 - Flow Controls/1.2 - Loop/e5tfs.java
@@ -1,9 +1,8 @@
-// Exercises on Loops (Iteration). 5 - Coza Loza Woza
-//
-
-/* Prints the numbers 1 to 110, eleven numbers per line.
-It prints "Coza" in place of multiples of 3, "Loza" for multiples of 5,
-and "Woza" for multiples of 7. "CozaLoza" for multiples of 3 and 5, and so on. */
+/***
+* Prints the numbers 1 to 110, eleven numbers per line.
+* It prints "Coza" in place of multiples of 3, "Loza" for multiples of 5,
+* and "Woza" for multiples of 7. "CozaLoza" for multiples of 3 and 5, and so on. 
+*/
 
 /* public class e5tfs {
 	public static void main(String[] args) {
@@ -44,42 +43,47 @@ and "Woza" for multiples of 7. "CozaLoza" for multiples of 3 and 5, and so on. *
 	}
 }
  */
-// Try: Modify the program to use nested-if loops instead:
-// Note: write out the T-F decision tree for each case, and work
-// with the visual aid.
+ 
+/**
+* Try: Modify the program to use nested-if loops instead:
+* Note: write out the T-F decision tree for each case, and work
+* with the visual aid.
+*/
 
 public class e5tfs {
+	
 	public static void main(String[] args) {
 		int lowerbound = 1;
 		int upperbound = 110;
-		for (int number = lowerbound; number<=upperbound; ++number) {
-			if (number%3 == 0) {
+		
+		for (int number = lowerbound; number <= upperbound; ++number) {
+			if (number % 3 == 0) {
 				System.out.print("Coza");
-				if (number%5 == 0) {
+				if (number % 5 == 0) {
 					System.out.print("Loza");
-					if (number%7 == 0) {
+					if (number % 7 == 0) {
 						System.out.print("Woza ");
 					} else {
 						System.out.print(" ");
 					}
-				} else if (number%7 ==0) {
+				} else if (number % 7 == 0) {
 					System.out.print("Woza ");
 				} else {
 					System.out.print(" ");
 				}
-			} else if (number%5 == 0) {
+			} else if (number % 5 == 0) {
 				System.out.print("Loza");
-				if (number%7 == 0) {
+				if (number % 7 == 0) {
 					System.out.print("Woza ");
 				} else {
 					System.out.print(" ");
 				}
-			} else if (number%7 == 0) {
+			} else if (number % 7 == 0) {
 				System.out.print("Woza ");
 			} else {
 				System.out.print(number+" ");
 			}
-			if (number%11 == 0) {
+			if (number % 11 == 0) {
 				System.out.println();
 			}
 		}
--- Exercises/01 - Flow Controls/1.2 - Loop/e6fibonacci.java
+++ Exercises/01 - Flow Controls/1.2 - Loop/e6fibonacci.java
@@ -1,8 +1,9 @@
-// Exercises on Loops (Iteration). 6 - Fibonacci
-//
-// compute the first 20 fibonacci numbers, and find their average
+/**
+* compute the first 20 fibonacci numbers, and find their average
+*/
 
 public class e6fibonacci {
+	
 	public static void main(String[] args) {
 		int n = 3;
 		int fn;
@@ -11,23 +12,27 @@ public class e6fibonacci {
 		int nMax = 20;
 		int sum = fnMinus1 + fnMinus2;
 		double average;
+		
 		System.out.println("The first " + nMax + " Fibonacci numbers are: ");
 		System.out.print(fnMinus1 + " ");
 		System.out.print(fnMinus2 + " ");
 		double totalsum = 0;
+		
 		while (n <= nMax) {
 			totalsum += sum;
 			System.out.print(sum+" "); // takes the sum from the previous iteration,
-			// below we adjust for the new index:
-			// example, if n = 4, f(4) = f(3) + f(2),
-			// f(3) = f(1) + f(2),
+			/** below we adjust for the new index:
+			* example, if n = 4, f(4) = f(3) + f(2),
+			* f(3) = f(1) + f(2)
+			*/
 			fnMinus2 = fnMinus1;
 			fnMinus1 = sum;
 			sum = fnMinus1 + fnMinus2;
 			n++;
 		}
-		// compute and display the average
-		System.out.println();
-		System.out.println("The average is "+totalsum/nMax);
+		/**
+		* compute and display the average
+		*/
+		System.out.println("\nThe average is " + totalsum / nMax);
 	}
 }
