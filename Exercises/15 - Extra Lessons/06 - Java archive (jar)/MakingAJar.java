474d8199d62413c632ee64d4f56cee5dcba0113c
mdnu <mark@sdentertainment.ca>
2016-04-23 14:16:29 -0400

    Making a jar, lesson.

---
+++ Exercises/15 - Extra Lessons/06 - Java archive (jar)/MakingAJar.java
@@ -0,0 +1,13 @@
+/**
+* making a jar:
+* in console
+* jar cvf myjarfile.jar c1.class ... cn.class
+* example: jar cvf hellopackage.jar com\zzz\test\HelloPackage.class
+*/
+
+public class MakingAJar {
+	
+	public static void main(String[] args) {
+		System.out.println("nothing here.");
+	}
+}
