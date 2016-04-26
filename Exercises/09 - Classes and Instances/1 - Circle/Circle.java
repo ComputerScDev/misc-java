abce45a5c736f572adb121b01bed8100b7c4b796
mdnu <mark@sdentertainment.ca>
2016-04-26 12:26:37 -0400

    Code cleaning.

--- Exercises/09 - Classes and Instances/1 - Circle/Circle.java
+++ Exercises/09 - Classes and Instances/1 - Circle/Circle.java
@@ -1,64 +1,85 @@
-//	Circle model class
-//	-radius:double
-//	-colour:String
-//	+Circle()
-//	+Circle(radius:double)
-//	+Circle(radius:double, colour:String)
-//	+getRadius():double
-//	+getColour():String
-//	+setRadius(radius:double):void
-//	+setColour(colour:String):void
-//	+getArea():double
-//	+getPerimiter():double
-//	+toString():String
+
+/**
+* Circle model class
+* -radius:double
+* -colour:String
+* +Circle()
+* +Circle(radius:double)
+* +Circle(radius:double, colour:String)
+* +getRadius():double
+* +getColour():String
+* +setRadius(radius:double):void
+* +setColour(colour:String):void
+* +getArea():double
+* +getPerimiter():double
+* +toString():String
+*/
 
 public class Circle {
 	
-	// private instance variables, not accessible from outside this class
+	/**
+	* private instance variables, not accessible from outside this class
+	*/
 	private double radius;
 	private String colour;
 	
-	// 1st constructor, which sets both radius & colour to default
+	/**
+	* 1st constructor, which sets both radius & colour to default
+	*/
 	public Circle() {
 		this.radius = 1.0;
 		this.colour = "red";
 	}
 	
-	// 2nd constructor, with given radius but colour default
+	/**
+	* 2nd constructor, with given radius but colour default
+	*/
 	public Circle(double radius) {
 		this.radius = radius;
 		this.colour = "red";
 	}
-
-	// 3rd constructor, constructs a Circle instance with given radius and colour
+	
+	/**
+	* 3rd constructor, constructs a Circle instance with given radius and colour
+	*/
 	public Circle(double radius, String colour) {
 		this.radius = radius;
 		this.colour = colour;
 	}
 	
-	// Getters
-	// a public method for getting the radius value:
+	/**
+	* Getters
+	* a public method for getting the radius value:
+	*/
 	public double getRadius() {
 		return radius;
 	}
 	
-	// a public method for getting the circle :
+	/**
+	* a public method for getting the circle :
+	*/
 	public String getColour() {
 		return colour;
 	}
 	
-	// Setters:
-	// a public setter for instance variable radius:
+	/**
+	* Setters:
+	* a public setter for instance variable radius:
+	*/
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
 	
-	// a public setter for instance variable colour:
+	/**
+	* a public setter for instance variable colour:
+	*/
 	public void setColour(String colour) {
 		this.colour = colour;
 	}
 	
-	// a public method for computing the circle's area
+	/**
+	* a public method for computing the circle's area
+	*/
 	public double getArea() {
 		return radius*radius*Math.PI;
 	}
