c92993919f04350fe38b351c18558ef0b09a33aa
mdnu <mdnu@users.noreply.github.com>
2016-04-30 23:37:49 -0400

    Update Cylinder.java

--- Exercises/10 - Inheritance/Circle and Cylinder/Cylinder.java
+++ Exercises/10 - Inheritance/Circle and Cylinder/Cylinder.java
@@ -1,43 +1,33 @@
-//	Cylinder model subclass of Circle superclass
-//	-height:double = 1.0
-//	+Cylinder()
-//	+Cylinder(height:double)
-//	+Cylinder(radius:double, height:double)
-//	+getHeight():double
-//	+getVolume():double
-
 public class Cylinder extends Circle {
 	
 	private static final double DEFAULT_HEIGHT = 1.0;
 	private double height;
 	
-	//	constructors
-	//	+Cylinder()
+	/**
+	 * constructors
+	 * /
 	public Cylinder() {
 		super();	// invokes superclass' no-arg constructor
 		height = DEFAULT_HEIGHT;
 	}
 	
-	//	+Cylinder(height:double)
 	public Cylinder(double height) {
 		super();	// invokes superclass' no-arg constructor
 		this.height = height;
 	}
 	
-	//	+Cylinder(radius:double, height:double)
 	public Cylinder(double radius, double height) {
 		super(radius);	// invokes superclass' second constructor
 		this.height = height;
 	}
 	
-	//	public methods
-	//	+getHeight():double
+	/**
+	 * public methods
+	 * /
 	public double getHeight() {
 		return height;
 	}
 	
-	//	+getVolume():double
-	//	V = pi*r^2*h
 	public double getVolume() {
 		return getBaseArea()*height;
 	}
