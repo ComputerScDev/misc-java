248ce68792667e74b38e05f41277e5df19bcf5a6
mdnu <mdnu@users.noreply.github.com>
2016-05-02 22:24:43 -0400

    Update Shape.java

--- Exercises/10 - Inheritance/Shape superclass/Shape.java
+++ Exercises/10 - Inheritance/Shape superclass/Shape.java
@@ -1,14 +1,3 @@
-//	Shape model superclass
-//	-colour:String = "green"
-//	-filled:boolean = "true"
-//	+Shape()
-//	+Shape(colour:String, filled:boolean)
-//	+getColour():String
-//	+setColour(colour:String):void
-//	+isFilled():boolean
-//	+setFilled(filled:boolean):void
-//	+toString():String
-
 public class Shape {
 	
 	private String colour;
@@ -16,42 +5,41 @@ public class Shape {
 	private static final String DEFAULT_COLOUR = "green";
 	private static final boolean DEFAULT_FILLED = true;
 	
-	//	constructors
-	//	+Shape()
+	/**
+	 * constructors
+	 */
 	public Shape() {
 		colour = DEFAULT_COLOUR;
 		filled = DEFAULT_FILLED;
 	}
-	
-	//	+Shape(colour:String, filled:boolean)
+
 	public Shape(String colour, boolean filled) {
 		this.colour = colour;
 		this.filled = filled;
 	}
 	
-	//	getters
-	//	+getColour():String
+	/**
+	 * getters and setters
+	 */
 	public String getColour() {
 		return colour;
 	}
 	
-	//	+isFilled():boolean
 	public boolean isFilled() {
 		return filled;
 	}
 	
-	//	setters
-	//	+setColour(colour:String):void
 	public void setColour(String colour) {
 		this.colour = colour;
 	}
 	
-	//	+setFilled(filled:boolean):void
 	public void setFilled(boolean filled) {
 		this.filled = filled;
 	}
 	
-	//	+toString():String
+	/**
+	 * public method
+	 */
 	public String toString() {
 		return "a shape of colour " + colour + " which " + (isFilled() ? "is filled." : "is not filled.");
 	}
