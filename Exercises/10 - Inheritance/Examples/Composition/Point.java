6cc1e0e861c4f38ea0015f7d0a226530d97d128d
mdnu <mdnu@users.noreply.github.com>
2016-05-03 22:41:55 -0400

    Update Point.java

--- Exercises/10 - Inheritance/Examples/Composition/Point.java
+++ Exercises/10 - Inheritance/Examples/Composition/Point.java
@@ -1,18 +1,8 @@
-//	Composition example
-//	Point model class
-//	-x:int
-//	-y:int
-//	+Point()
-//	+Point(x:int, y:int)
-//	+getX():int
-//	+getY():int
-//	+setX(x:int):void
-//	+setY(y:int):void
-//	+toString():String
-
 public class Point {
 	
-	//	private member fields (variables);
+	/**
+	 * private member fields (variables)
+	 */
 	private int x, y;
 	
 	//	constructors
@@ -22,34 +12,33 @@ public class Point {
 		this.y = y;
 	}
 	
-	//	default (no-arg) constructor
-	//	+Point(x:int, y:int)
+	/**
+	 * constructors
+	 */
 	public Point() {
 		this.x = 0;
 		this.y = 0;
 	}
 	
-	//	+getX():int
+	/**
+	 * getters and setters
+	 */
 	public int getX() {
 		return this.x;
 	}
 	
-	//	+getY():int
 	public int getY() {
 		return this.y;
 	}
 	
-	//	+setX(x:int):void
 	public void setX(int x) {
 		this.x = x;
 	}
 	
-	//	+setY(y:int):void
 	public void setY(int y) {
 		this.y = y;
 	}
 	
-	//	+toString():String
 	public String toString() {
 		return "(" + this.x + ", " + this.y + ")";
 	}
